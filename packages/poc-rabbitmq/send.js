3390ec3926e6a06bb874c0043984329ca1276b69
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-02-16 22:38:24 -0300

    🚧 sender

---
+++ packages/poc-rabbitmq/send.js
@@ -0,0 +1,16 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel((error1, channel1) => {
+    if (error1) {
+      throw error1;
+    }
+
+    channel1.assertQueue('hello', { durable: false });
+    channel1.sendToQueue('hello', Buffer.from('https://www.linkedin.com/in/marqueswsm/'));
+  });
+});
